eabdf98095ef08f8e764502758315a725679c75f
Ola Nordstrom <ola.nordstrom@gmail.com>
2013-01-17 14:57:24 -0500

    Updated 'list_display' for TeamAdmin to show all model members in the admin view.

--- rf/registration/admin.py
+++ rf/registration/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from models import Team
 
 class TeamAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('teamname', 'division', 'captain', 'email', 'phone_number', 'date', 'paid')
 
 admin.site.register(Team, TeamAdmin)
--- rf/registration/models.py
+++ rf/registration/models.py
@@ -15,7 +15,7 @@ class Team(models.Model):
     email = models.EmailField('Contact Email', blank=False, max_length=256, help_text='Please enter a valid email')
     phone_number = models.CharField('Phone Number', blank=False, max_length=20, null=True,
                                           help_text='Please enter a valid phone number')
-    date = models.DateTimeField('Date played', auto_now_add=True)
+    date = models.DateTimeField('Registration Date', auto_now_add=True)
     paid = models.BooleanField(blank=True, default=False)
     def __unicode__(self):
         return self.teamname
